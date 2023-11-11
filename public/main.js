862d1be47052af45758b0e9bbd66d74caae0cf2f
--global <--global>
2023-11-11 20:00:01 +0100

    init react app with electron

---
+++ public/main.js
@@ -0,0 +1,36 @@
+const path = require("path");
+const { app, BrowserWindow } = require("electron");
+const isDev = require("electron-is-dev");
+
+function createWindow() {
+  // Create the browser window.
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: true,
+    },
+  });
+
+  //load the index.html from a url
+  win.loadURL(
+    isDev ? "http://localhost:3000" : path.join(__dirname, "index.html")
+  );
+
+  // Open the DevTools.
+  win.webContents.openDevTools();
+}
+
+app.whenReady().then(createWindow);
+
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") {
+    app.quit();
+  }
+});
+
+app.on("activate", () => {
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createWindow();
+  }
+});
